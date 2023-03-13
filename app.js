cad28b8e24c2bf8946142ef6555b53f52018c110
Yushan Fernando <yushanwebdev@gmail.com>
2023-03-13 17:47:27 +0000

    feat: Custom Header & Status Code

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
  res.send('Nice work, Yushan!!!');
 });
 
+app.get('/about', (req, res) => {
+ res.status(418);
+ res.set('X-Yushan', 'Hello Friend')
+ res.send('atta-boy')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
